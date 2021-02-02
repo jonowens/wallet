5a543038d44d0661ecc150e3556726455fca997a
jowens520 <jonathan.owens@gmail.com>
2021-02-02 08:34:42 -0600

    Created test mnemonic with .env and can load test mnemonic in wallet.py with fallback.

--- wallet.py
+++ wallet.py
@@ -1,6 +1,14 @@
 import subprocess
 import json
 from constants import *
+import os
+from dotenv import load_dotenv
+
+# load .env environment variables
+load_dotenv()
+
+# set the mnemonic as an environment variable with a fallback
+mnemonic = os.getenv('MNEMONIC', 'balcony defense hurdle paddle crazy vivid decorate blast behind heavy exchange fat')
 
 command = './derive -g --mnemonic="INSERT HERE" --cols=path,address,privkey,pubkey --format=json'
 
